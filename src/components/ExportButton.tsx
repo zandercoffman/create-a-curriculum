06094220543466c42ca77e1a65c438600e33dce7
Zander Coffman <zandergalvez330@gmail.com>
2024-08-02 21:10:09 -0700

    Made a quick solution lol

--- src/components/ExportButton.tsx
+++ src/components/ExportButton.tsx
@@ -66,40 +66,43 @@ interface Props {
 export default function ExportButton(props: Props) {
 
     const [selIndex, setselIndex] = useState<number>(props.sel);
-    const [selected, setSelected] = useState<number>(() => {
-        const savedIndex = localStorage.getItem("selectedCurriculumIndex");
-        if (!savedIndex) {
-            localStorage.setItem("selectedCurriculumIndex", "0");
-            return 0;
-        } else {
-            return parseInt(savedIndex)
-        }
-    });
+    const [selected, setSelected] = useState<number>(0);
     const [canedit, setedit] = useState(false)
 
     const { toast } = useToast();
 
 
     const [messageContent, setMessageContent] = useState<string>("");
-    const [curriculumContent, setCurriculumContent] = useState<string | null>(() => {
-        const m = localStorage.getItem("messageData");
-        if (m) {
-            const j = JSON.parse(m);
-            const sel = j[selected];
-            const messages = sel.messages;
-            return messages[messages.length - 1].content;
-        } else {
-            return "";
+    const [curriculumContent, setCurriculumContent] = useState<string | null>(null);
+
+    useEffect(() => {
+        const a = () => {
+            const savedIndex = localStorage.getItem("selectedCurriculumIndex");
+            if (!savedIndex) {
+                localStorage.setItem("selectedCurriculumIndex", "0");
+                return 0;
+            } else {
+                return parseInt(savedIndex)
+            }
+        };
+        setSelected(a());
+        
+        const g = () => {
+            const m = localStorage.getItem("messageData");
+            if (m) {
+                const j = JSON.parse(m);
+                const sel = j[selected];
+                const messages = sel.messages;
+                return messages[messages.length - 1].content;
+            } else {
+                return null;
+            }
         }
-    });
 
-    const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setMessageContent(e.target.value);
-    };
+        setCurriculumContent(g());
 
-    const handleCurriculumChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setCurriculumContent(e.target.value);
-    };
+        
+    }, [selected])
 
     const [loading, setLoading] = useState(false);
 
@@ -115,139 +118,6 @@ export default function ExportButton(props: Props) {
             setCurriculumContent((goodm.content))
         }
     }, [selIndex])
-
-    const sho = () => {
-        if (props.userData !== null) {
-            return `
-                Applicant Name: ${props.userData.name}
-                ID: ${props.userData.uniqueid}
-            `
-        } else {
-            return ''
-        }
-    }
-
-    const handleExportPDF = async () => {
-        console.log("Export to PDF clicked");
-        setLoading(true);
-
-        // Define form1Data and form2Data
-        const form1Data: FORM1 | null = props.userData || null; // Ensure props.userData matches FORM1 type or set to null
-        const c = await remark().use(strip).process(curriculumContent as string);
-        const form2Data = curriculumContent;
-
-        try {
-            // Send a POST request with form1 and form2 data
-            const response = await fetch('/api/generate-pdf', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    form1: form1Data,
-                    form2: form2Data
-                })
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-            // Create a blob from the response
-            const blob = await response.blob();
-
-            // Create a URL for the blob and trigger the download
-            const url = window.URL.createObjectURL(blob);
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', 'example.pdf'); // Set the filename for the download
-            document.body.appendChild(link);
-            link.click();
-
-            // Cleanup
-            link.remove();
-            window.URL.revokeObjectURL(url);
-            toast({
-                title: "Your .PDF file has been generated.",
-                description: "Enjoy using it!"
-            });
-        } catch (error: any) {
-            console.error('Error downloading PDF:', error);
-            toast({
-                title: "Error generating PDF",
-                description: "There was a problem generating your PDF. Error: " + error.message
-            });
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const handleExportText = async () => {
-        console.log("Export to DOCX clicked");
-        setLoading(true);
-
-        const form1Data: FORM1 | null = props.userData || null;
-        const form2Data = curriculumContent;
-
-        try {
-            const response = await fetch('/api/generate-doc', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    form2: form2Data,
-                    form1: form1Data,
-                }),
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-
-            // Convert response to blob and initiate download using file-saver
-            const blob = await response.blob();
-            saveAs(blob, 'curriculum.docx'); // Save the file with file-saver
-
-            toast({
-                title: "Your .DOC file has been generated.",
-                description: "Enjoy using it!"
-            });
-        } catch (error: any) {
-            console.error('Error downloading document:', error);
-            toast({
-                title: "Error generating DOCX",
-                description: "There was a problem generating your DOCX. Error: " + error.message
-            });
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const handleCopy = () => {
-        console.log("Copy clicked");
-        setLoading(true);
-
-        const form1Data: FORM1 | null = props.userData || null;
-        const form2Data = curriculumContent;
-
-        try {
-            navigator.clipboard.writeText(
-                JSON.stringify(form1Data || "" + " " + form2Data).replace(/[^a-zA-Z0-9:]/g, ' ').replace(/\t/g, '')
-            ).then(() => {
-                toast({
-                    title: "Successfully copied!",
-                    description: "Enjoy using your curriculum!"
-                })
-            })
-        } catch (err) {
-
-        } finally {
-            setLoading(false);
-        }
-
-    };
-
 
     return <>
         <Sheet>
@@ -275,81 +145,7 @@ export default function ExportButton(props: Props) {
                         Check to see your outputted curriculum. Feel free to edit parts you would like to change.
                     </SheetDescription>
                 </SheetHeader>
-                <Tabs defaultValue="messages" className="w-full h-full">
-                    <TabsList className="w-full">
-                        <TabsTrigger value="messages" className="w-1/2">Messages</TabsTrigger>
-                        <TabsTrigger value="overview" className="w-1/2">Overview</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="overview">
-                        {
-                            curriculumContent == null ? <>
-                                <div className="w-full grid h-[70vh] lg:h-[65vh] place-items-center">
-                                    <div className="flex flex-col gap-2 items-center text-center ">
-                                        <CircleSlash2 className="w-10 h-10" />
-                                        <span className="text-2xl">We couldn{"'"}t locate any curriculums at the moment. Please check back later.</span>
-                                        <span className="text-1xl"><span className="font-bold">Note:</span> Curriculums may not update immediately. Please try pressing to a different section.</span>
-                                    </div>
-                                </div>
-                            </> : <>
-                                <div className="flex flex-col h-[70vh] lg:h-[62vh] py-2 gap-2 mx-auto">
-                                    {
-                                        props.userData !== null && <>
-                                            <div className=" w-full h-[30%] gap-1.5">
-                                                <Label htmlFor="message">Your information</Label>
-                                                <Textarea
-                                                    placeholder="Type your message here."
-                                                    id="message"
-                                                    value={sho()}
-                                                    onChange={handleMessageChange}
-                                                    readOnly={!canedit}
-                                                    className="h-[98%]"
-                                                />
-                                            </div>
-                                        </>
-                                    }
-                                    <div className=" w-full h-[50%] gap-1.5">
-                                        <Label htmlFor="curriculum">Your curriculum</Label>
-                                        <Textarea
-                                            placeholder="Type your message here."
-                                            id="curriculum"
-                                            value={curriculumContent as string}
-                                            onChange={handleCurriculumChange}
-                                            readOnly={!canedit}
-                                            className="h-[98%]"
-                                        />
-                                    </div>
-                                    <Badge className="mx-auto">Selected Message: Curriculum #{selIndex + 1}</Badge>
-                                    <div className="w-full h-[20%] flex flex-row gap-2">
-                                        {
-                                            loading ? <>
-                                                <div className="grid place-items-center w-full h-full" >
-                                                    <div className="flex flex-row gap-2 mx-auto my-auto text-center">
-                                                        <Loader className="animate-spin" />
-                                                        <p>Your file is currently being generated!</p>
-                                                    </div>
-                                                </div>
-                                            </> : <>
-                                                <BigButton text="Export to .PDF" icon={FileText} callback={handleExportPDF} />
-                                                <BigButton text="Export to .DOC" icon={Text} callback={handleExportText} />
-                                                <BigButton text="Copy" icon={ClipboardCopy} callback={handleCopy} />
-                                            </>
-                                        }
-
-
-                                    </div>
-                                    <Button onClick={() => setedit(!canedit)}>Toggle Editable (Currently: {canedit ? 'Can Edit' : 'Cannot edit'})</Button>
-                                    <div className="text-center">
-                                        (.doc is the best format as of now)
-                                    </div>
-                                </div>
-                            </>
-                        }
-
-                    </TabsContent>
-                    <TabsContent value="messages">
-                        <MessageSelector setselIndex={setselIndex} selIndex={selIndex} />
-                    </TabsContent>
-                </Tabs>
+                <MessageSelector setselIndex={setselIndex} selIndex={selIndex} />
             </SheetContent>
         </Sheet>
     </>
--- src/components/MessageSelector.tsx
+++ src/components/MessageSelector.tsx
@@ -14,11 +14,11 @@ import {
     CarouselPrevious,
     type CarouselApi,
 } from "@/components/ui/carousel"
-import React, { useState } from "react"
+import React, { SVGProps, useState } from "react"
 import { Button } from "./ui/button"
 import { ScrollArea } from "./ui/scroll-area"
 import { Message } from "ai"
-import { CircleSlash2, ClipboardCopy, LoaderCircle, SquareArrowOutUpRight } from "lucide-react"
+import { CircleSlash2, ClipboardCopy, FileText, LoaderCircle, SquareArrowOutUpRight, Text } from "lucide-react"
 import Markdown from "react-markdown"
 import {
     Dialog,
@@ -28,6 +28,33 @@ import {
 } from "@/components/ui/dialog"
 import { useToast } from "./ui/use-toast"
 import { Badge } from "./ui/badge"
+import { remark } from 'remark'
+import strip from 'strip-markdown'
+import { saveAs } from 'file-saver';
+
+interface FORM1 {
+    name: string;
+    uniqueid: string;
+    grade: number | undefined | string;
+    saveinfo: boolean;
+    wantstousegrade: boolean;
+}
+
+interface FORM2TITLES {
+    titles: string;
+}
+
+interface FORM2PRODUCT {
+    name: string;
+    description: string;
+    lessons: number;
+}
+
+interface LINKStorage {
+    name: string;
+    link: string;
+    id: string;
+}
 
 interface MessageDat {
     id: string;
@@ -83,8 +110,20 @@ export default function MessageSelector(props: any) {
     }, [api]);
 
     React.useEffect(() => {
-        props.setselIndex(current - 1);
-    }, [current, props]);
+        try {
+            const m = localStorage.getItem("messageData");
+            if (m) {
+                const j = JSON.parse(m);
+                const sel = j[current - 1];
+                const messages = sel.messages;
+                setCurriculumContent(messages[messages.length - 1].content || "");
+            }
+        } catch (err) {
+
+        }
+    }, [current]);
+
+    const [curriculumContent, setCurriculumContent] = useState<string | null>(null);
 
     React.useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -109,6 +148,127 @@ export default function MessageSelector(props: any) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
+    const handleExportPDF = async () => {
+        console.log("Export to PDF clicked");
+        setLoading(true);
+
+        // Define form1Data and form2Data
+        const form1Data: FORM1 | null = props.userData || null; // Ensure props.userData matches FORM1 type or set to null
+        const c = await remark().use(strip).process(curriculumContent as string);
+        const form2Data = curriculumContent;
+
+        try {
+            // Send a POST request with form1 and form2 data
+            const response = await fetch('/api/generate-pdf', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    form1: form1Data,
+                    form2: form2Data
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            // Create a blob from the response
+            const blob = await response.blob();
+
+            // Create a URL for the blob and trigger the download
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'example.pdf'); // Set the filename for the download
+            document.body.appendChild(link);
+            link.click();
+
+            // Cleanup
+            link.remove();
+            window.URL.revokeObjectURL(url);
+            toast({
+                title: "Your .PDF file has been generated.",
+                description: "Enjoy using it!"
+            });
+        } catch (error: any) {
+            console.error('Error downloading PDF:', error);
+            toast({
+                title: "Error generating PDF",
+                description: "There was a problem generating your PDF. Error: " + error.message
+            });
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleExportText = async () => {
+        console.log("Export to DOCX clicked");
+        setLoading(true);
+
+        const form1Data: FORM1 | null = props.userData || null;
+        const form2Data = curriculumContent;
+
+        try {
+            const response = await fetch('/api/generate-doc', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    form2: form2Data,
+                    form1: form1Data,
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            // Convert response to blob and initiate download using file-saver
+            const blob = await response.blob();
+            saveAs(blob, 'curriculum.docx'); // Save the file with file-saver
+
+            toast({
+                title: "Your .DOC file has been generated.",
+                description: "Enjoy using it!"
+            });
+        } catch (error: any) {
+            console.error('Error downloading document:', error);
+            toast({
+                title: "Error generating DOCX",
+                description: "There was a problem generating your DOCX. Error: " + error.message
+            });
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleCopy = () => {
+        console.log("Copy clicked");
+        setLoading(true);
+
+        const form1Data: FORM1 | null = props.userData || null;
+        const form2Data = curriculumContent;
+
+        try {
+            navigator.clipboard.writeText(
+                JSON.stringify(form1Data || "" + " " + form2Data).replace(/[^a-zA-Z0-9:]/g, ' ').replace(/\t/g, '')
+            ).then(() => {
+                toast({
+                    title: "Successfully copied!",
+                    description: "Enjoy using your curriculum!"
+                })
+            })
+        } catch (err) {
+
+        } finally {
+            setLoading(false);
+        }
+
+    };
+
     if (cant) {
         return <>
             <div className="w-full grid h-[70vh] lg:h-[65vh] place-items-center">
@@ -132,10 +292,8 @@ export default function MessageSelector(props: any) {
     }
 
     return <>
-        hello
-        {selected}
-        <div className="flex flex-col h-[70vh] lg:h-[65vh]  w-[80%] mx-auto gap-3 py-2">
-            <Carousel setApi={setApi} className="w-full max-w-xs">
+        <div className="flex flex-col h-[80vh] lg:h-[69vh]  mx-auto gap-3 py-2">
+            <Carousel setApi={setApi} className="w-[80%] mx-auto">
                 <CarouselContent>
                     {data.map((dat, index) => (
                         <CarouselItem key={index}>
@@ -180,12 +338,12 @@ export default function MessageSelector(props: any) {
                 <CarouselNext />
             </Carousel>
             <div className="py-2 text-center text-sm text-muted-foreground">
-                Slide {current} of {count}
+                Curriculum {current} of {count}
             </div>
-            <div className="mt-auto">
-                <Button className="w-full" variant={selected === current - 1 ? 'secondary' : 'default'}
-                    onClick={() => handleSelect(current - 1)}
-                >{selected === current - 1 ? 'Currently Selected' : 'Select'}</Button>
+            <div className="mt-auto flex flex-row gap-2">
+                <BigButton text="Export to .PDF" icon={FileText} callback={handleExportPDF} />
+                <BigButton text="Export to .DOC" icon={Text} callback={handleExportText} />
+                <BigButton text="Copy" icon={ClipboardCopy} callback={handleCopy} />
             </div>
         </div>
     </>
@@ -249,3 +407,17 @@ export default function MessageSelector(props: any) {
             </div>
         </div>
  */
+interface BigButtonProps {
+    text: string;
+    icon: React.ComponentType<SVGProps<SVGSVGElement>>;
+    callback?: () => void; // Optional callback function
+}
+
+function BigButton({ text, icon: Icon, callback }: BigButtonProps) {
+    return (
+        <Button className="w-1/3 h-max flex flex-col items-center justify-center my-auto gap-1 py-2" onClick={callback}>
+            <Icon className="w-6 h-6" /> {/* Adjust icon size as needed */}
+            <p>{text}</p>
+        </Button>
+    );
+}
